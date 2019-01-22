b040bb03c504fc95ce3fc412675ad1e8f0cccaf9
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-01-22 19:43:57 +0300

    update2

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 # Create your views here.
 def default(request):
-	context = {"msg": "Hello World",}
+	context = {"msg": "Hello World!",}
 	#return HttpResponse("<h1>{{msg}}</h1>")
 	return render(request,'default.html',context)
